408311e24fc086e2388a80b772b6677b14b6384a
brian <bcburnett@gmail.com>
2020-04-03 10:21:22 -0400

    initial commit

---
+++ __main__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  3 10:13:55 2020
+
+@author: brian
+"""
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello world'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
