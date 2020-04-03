02395d9df24c6c5a52f770fc60cb711912388bbc
brian <bcburnett@gmail.com>
2020-04-03 11:28:50 -0400

    python multi line string literal

--- __main__.py
+++ __main__.py
@@ -17,7 +17,20 @@ def index():
 
 @app.route('/cakes')
 def cakes():
-    return 'Yummy cakes!'
+    
+    name = 'Brian'
+    html=f"""
+<html>
+<body>
+<h1>hello {name}</h1>
+<h2> We Have Some Yummy Cakes
+</body>
+</html>
+"""
+    return html
+    
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+    
+    
